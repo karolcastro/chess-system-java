a648bebd4bfc95f60228d117f9358d469a2c8df5
Karol de Castro <karolinycosta@yahoo.com.br>
2021-08-25 15:53:50 -0300

    checkmate logic and piece move count

--- src/main/java/br/com/jogoDeXadrez/chess/ChessPiece.java
+++ src/main/java/br/com/jogoDeXadrez/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import br.com.jogoDeXadrez.boardGame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -16,9 +17,21 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
+    public int getMoveCount(){
+        return moveCount;
+    }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+
     protected boolean isThereOpponentPiece(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
-        return  p != null  && p.color != color;
+        return p != null && p.color != color;
     }
 
     public ChessPosition getChessPosition() {
